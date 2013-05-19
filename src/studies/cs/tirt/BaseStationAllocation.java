a53c34433525d950aac3fb0e97a61dee2fad2a2b
DamianKowalczyk <damian13kowalczyk@gmail.com>
2013-05-19 18:23:30 +0200

    deleted comented code

--- src/studies/cs/tirt/BaseStationAllocation.java
+++ src/studies/cs/tirt/BaseStationAllocation.java
@@ -74,17 +74,73 @@ public class BaseStationAllocation extends JFrame {
 		gbcCTR.fill = GridBagConstraints.BOTH;
 		contentPane.add(ctrPanel, gbcCTR);		
 		GridBagLayout gbl_ctrPanel = new GridBagLayout();
-		gbl_ctrPanel.columnWidths = new int[]{0, 0};
-		gbl_ctrPanel.rowHeights = new int[]{0, 0};
-		gbl_ctrPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_ctrPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_ctrPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_ctrPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_ctrPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_ctrPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		ctrPanel.setLayout(gbl_ctrPanel);
 		
 		JLabel lblNewLabel = new JLabel("Choosen algorithm:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
+		gbc_lblNewLabel.gridy = 1;
 		ctrPanel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JRadioButton rdbtnAlgorithm = new JRadioButton("Algorithm1");
+		GridBagConstraints gbc_rdbtnAlgorithm = new GridBagConstraints();
+		gbc_rdbtnAlgorithm.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnAlgorithm.gridx = 0;
+		gbc_rdbtnAlgorithm.gridy = 2;
+		ctrPanel.add(rdbtnAlgorithm, gbc_rdbtnAlgorithm);
+		
+		JRadioButton rdbtnAlgorithm_1 = new JRadioButton("Algorithm2");
+		GridBagConstraints gbc_rdbtnAlgorithm_1 = new GridBagConstraints();
+		gbc_rdbtnAlgorithm_1.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnAlgorithm_1.gridx = 0;
+		gbc_rdbtnAlgorithm_1.gridy = 3;
+		ctrPanel.add(rdbtnAlgorithm_1, gbc_rdbtnAlgorithm_1);
+		
+		JRadioButton rdbtnAlgorithm_2 = new JRadioButton("Algorithm3");
+		GridBagConstraints gbc_rdbtnAlgorithm_2 = new GridBagConstraints();
+		gbc_rdbtnAlgorithm_2.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnAlgorithm_2.gridx = 0;
+		gbc_rdbtnAlgorithm_2.gridy = 4;
+		ctrPanel.add(rdbtnAlgorithm_2, gbc_rdbtnAlgorithm_2);
+		
+		JLabel lblRating = new JLabel("Rating");
+		GridBagConstraints gbc_lblRating = new GridBagConstraints();
+		gbc_lblRating.insets = new Insets(0, 0, 5, 5);
+		gbc_lblRating.gridx = 0;
+		gbc_lblRating.gridy = 8;
+		ctrPanel.add(lblRating, gbc_lblRating);
+		
+		JLabel lblFirstRanking = new JLabel("first ranking");
+		GridBagConstraints gbc_lblFirstRanking = new GridBagConstraints();
+		gbc_lblFirstRanking.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFirstRanking.gridx = 0;
+		gbc_lblFirstRanking.gridy = 10;
+		ctrPanel.add(lblFirstRanking, gbc_lblFirstRanking);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 10;
+		ctrPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JLabel lblSecondRanking = new JLabel("second ranking");
+		GridBagConstraints gbc_lblSecondRanking = new GridBagConstraints();
+		gbc_lblSecondRanking.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSecondRanking.gridx = 0;
+		gbc_lblSecondRanking.gridy = 11;
+		ctrPanel.add(lblSecondRanking, gbc_lblSecondRanking);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 11;
+		ctrPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		gbcArea.gridx = 1;
 		gbcArea.gridy = 0;
@@ -93,37 +149,6 @@ public class BaseStationAllocation extends JFrame {
 		gbcArea.fill = GridBagConstraints.BOTH;
 		contentPane.add(areaPanel, gbcArea);
 		
-		/*JRadioButton rdbtnNewRadioButton = new JRadioButton("Algorithm1");
-		ctrPanel.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Algorithm2");
-		ctrPanel.add(rdbtnNewRadioButton_1);
-		
-		JRadioButton rdbtnAlgorithm = new JRadioButton("Algorithm3");
-		ctrPanel.add(rdbtnAlgorithm);*/
-		
-		
-		
-		//contentPane.add(areaPanel, gbc);
-		
-		/*GridBagConstraints c = new GridBagConstraints();
-		JButton button;
-		button = new JButton("Button 1");
-
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.weightx = 0.3;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weighty = 1.0;
-		contentPane.add(button, gbc);
-
-		button = new JButton("Button 2");
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 0.7;
-		c.gridx = 1;
-		c.gridy = 0;
-		c.weighty = 1.0;
-		contentPane.add(button, c);*/
 		
 	}
 }
